009e977325da7b2cfd0dbf434eee3bfc6ccfa7de
GeovaneReboucas <geovane.reboucas2002@gmail.com>
2022-03-27 08:03:19 -0300

    first commit

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas/models/todo.dart';
+import 'package:lista_tarefas/widgets/Todo_list_item.dart';
+
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  List<Todo> todos = [];
+
+  final TextEditingController todoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 45, left: 16, right: 16, bottom: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todoController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex: Estudar flutter',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      String text = todoController.text;
+                      setState(() {
+                        Todo newTodo = Todo(
+                          title: text,
+                          dateTime: DateTime.now(),
+                        );
+                        todos.add(newTodo);
+                      });
+                      todoController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00b5f4),
+                      padding: const EdgeInsets.all(13),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (Todo todo in todos)
+                      TodoListItem(
+                        todo: todo,
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text('Você possui ${todos.length} tarefas pendentes'),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Limpar tudo'),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00b5f4),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/Todo_list_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/todo.dart';
+
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({Key? key, required this.todo}) : super(key: key);
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Colors.grey[200],
+      ),
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 3),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+            style: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
+          Text(
+            todo.title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
